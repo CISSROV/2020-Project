5b6ffb9b6459cc14207daacb8bd0096f045a2572
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-03 18:51:35 +0800

    Update startDataCollection.sh

--- code/sensors/startDataCollection.sh
+++ code/sensors/startDataCollection.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
 cd /var/www/scripts
+
+if [ $? -eq 0 ]
+then
+    echo 'OK'
+else
+    echo 'Error Code' $?
+    exit 1
+fi
+
 nohup sudo python3.4 dataCollection.py > out.log &
+
+if [ $? -eq 0 ]
+then
+    echo 'OK'
+else
+    echo 'Error Code' $?
+    exit 1
+fi
 
 tmp=$(pgrep -f "sudo python3.4 dataCollection.py")
 
