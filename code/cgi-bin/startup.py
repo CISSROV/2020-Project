a973e40559a6ef752b7767006a4a2b0b89af889d
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-07 16:59:24 +0800

    Update startup.py

--- code/cgi-bin/startup.py
+++ code/cgi-bin/startup.py
@@ -3,10 +3,11 @@ import os
 
 print ("Content-type: text/html\n\n")
 
-file = '/var/www/scripts/startDataCollection.sh'
-result = os.popen(file).read()
+file = 'sudo /var/www/scripts/dataCollection.sh start'
+result = os.system(file)#.read()
 
 print('<h3>Starting Up dataGetter.py</h3>')
 print('<p>' + str(result) + '</p>')
 print('<br>')
 print('<a href=\'dataSheet.html\'>Link to Data Sheet</a>')
+
