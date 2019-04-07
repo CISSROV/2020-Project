987a8df161f713deefc88111de0a941fdda6d02a
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-07 13:44:27 +0800

    Create dataCollection.sh

---
+++ code/sensors/dataCollection.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This script combines startDataCollection.sh and stopDataCollection.sh into one file to reduce clutter
+
+if [[ $1 == 'start' ]]
+then
+    echo "Startup!"
+
+    cd /var/www/scripts
+
+    nohup sudo python3.4 dataCollection.py &
+
+    tmp=$(pgrep -f "sudo python3.4 dataCollection.py")
+
+    echo "PID: $tmp"
+
+elif [[ $1 == 'stop' ]]
+then
+    echo "Shutdown!"
+
+    tmp=$(pgrep -f "sudo python3.4 dataCollection.py")
+
+    if [ -z $tmp ]
+    then
+        echo "It seems that the code isn't running"
+        echo "Try usning ps -eaf | grep python to find it"
+        exit 1
+    else
+        echo PID: $tmp
+        sudo kill -s SIGINT $tmp
+        sleep 1
+        sudo kill -s SIGKILL $tmp
+    fi
+
+    tmp=$(pgrep -f "python3.4 dataCollection.py")
+
+    if [ -z $tmp ]
+    then
+        exit 1
+    else
+        echo PID: $tmp
+        sudo kill -s SIGINT $tmp
+        sleep 1
+        sudo kill -s SIGKILL $tmp
+    fi
+else
+    echo "Usage: dataCollection.sh [start|stop]"
+fi
