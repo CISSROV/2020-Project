d96c2896943256176121f1f25e0e15e44b6ae790
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-16 21:31:08 +0800

    testing

--- code/core/startup.py
+++ code/core/startup.py
@@ -3,8 +3,16 @@
 import sys
 import subprocess
 
+pseudo = False
+
 def runShellCmd(cmd):
     pre = 'bash -c "{0}"'
+
+    global pseudo
+    if pseudo:
+        print(pre.format(cmd))
+        sys.exit(0)
+
     f = subprocess.Popen(pre.format(cmd), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     raw = f.communicate()
     f.stdout.close()
@@ -22,6 +30,10 @@ if len(sys.argv) <= 1:
     print('Usage: startup.py [start|stop|check]')
     sys.exit(1)
 
+if len(sys.argv) >= 3:
+    if sys.argv[2] == '--debug':
+        pseudo = True
+
 if sys.argv[1] in ['start', 'stop']:
     # run cmd and pass the argument along
     txt = runShellCmd(' '.join([baseCmd, script, sys.argv[1]]))
@@ -33,7 +45,7 @@ if sys.argv[1] in ['start', 'stop']:
 
 elif sys.argv[1] == 'check':
     print('Check')
-    check = 'pgrep -f "sudo python3.4 data"'
+    check = 'pgrep -f \"sudo python3.4 data\"'
     txt = runShellCmd(' '.join([baseCmd, check]))
     print(txt)
 
