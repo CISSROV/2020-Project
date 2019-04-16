cbaccb18a2284d2867184b366e8100e173ae6902
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-16 16:45:50 +0800

    Update startup.py

--- code/core/startup.py
+++ code/core/startup.py
@@ -3,5 +3,27 @@
 import os
 import sys
 
+def runShellCmd(cmd):
+    f = os.popen(cmd)
+    txt = f.read()
+    f.close()
+    return txt
+
+baseCmd = 'sshpass -p raspberry ssh pi@192.168.1.3 sudo /var/www/scripts/dataCollection.sh'
+
 if len(sys.argv) <= 1:
-    print('Usage: startup.py')
+    # no argument supplied
+    print('Usage: startup.py [start|stop]')
+    sys.exit(1)
+
+if sys.argv[1] in ['start', 'stop']:
+    # run cmd and pass the argument along
+    txt = runShellCmd(baseCmd + ' ' + sys.argv[1])
+    print(txt)
+
+else:
+    # unknown argument supplied
+    print('Usage: startup.py [start|stop]')
+    sys.exit(1)
+
+sys.exit(0)
