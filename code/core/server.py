999847a8af23df983940efece36712fb4c61dddc
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-21 21:50:11 +0800

    it works

--- code/core/server.py
+++ code/core/server.py
@@ -21,17 +21,12 @@ class ServerProtocol(WebSocketServerProtocol):
 
     def onConnect(self, request):
         print(request.path)
-        print(request)
         print('Client connecting & registering: {0}'.format(request.peer))
         clientTypeRequest = request.path
         if clientTypeRequest.startswith('/'):
             clientTypeRequest = clientTypeRequest[1:]
 
-        if clientTypeRequest not in clientTypes:
-            print('Bad client type received: {}'.format(clientTypeRequest))
-            self._closeConnection() # find smth better; unclean closing
-        else:
-            self.factory.register(self, clientTypeRequest)
+        self.factory.register(self, clientTypeRequest)
 
     def onOpen(self):
         print('WebSocket connection open')
@@ -41,29 +36,39 @@ class ServerProtocol(WebSocketServerProtocol):
         self.factory.unregister(self)
 
     def onMessage(self, msg, isBinary):
-        print(msg, isBinary)
+        self.factory.broadcast(self, msg, isBinary)
 
 class ServerFactory(WebSocketServerFactory):
 
     def __init__(self, url):
         WebSocketServerFactory.__init__(self, url)
-        self.clients = []
+        self.motorConnection = None
         self.surfaceConnection = None
 
     def register(self, client, clientTypeRequest):
-        if client not in self.clients:
-            self.clients.append(client)
+        if clientTypeRequest == 'surface':
+            self.surfaceConnection = client
+        elif clientTypeRequest == 'motor':
+            self.motorConnection = client
+        else:
+            print('Bad client type received: {}'.format(clientTypeRequest))
+            client._closeConnection() # find smth better; unclean closing
 
     def unregister(self, client):
-        if client in self.clients:
-            self.clients.remove(client)
+        if self.surfaceConnection == client:
+            self.surfaceConnection = None
+        elif self.motorConnection == client:
+            self.motorConnection = None
+        else:
+            print('Unknown client: {}'.format(client))
 
-    def broadcast(self):
-        msg = json.dumps(dataShards.getDataFragment())
-        print("broadcasting message '{}' ..".format(msg))
-        for c in self.clients:
-            c.sendMessage(msg.encode('utf8'))
-            print("message sent to {}".format(c.peer))
+    def broadcast(self, client, msg, isBinary):
+        if self.surfaceConnection == client:
+            # broadcasting
+            self.motorConnection.sendMessage(msg)
+        elif self.motorConnection == client:
+            print('Motor Pi isn\'t supposed to send stuff')
+
 
 log.startLogging(sys.stdout) # replace with log file
 
