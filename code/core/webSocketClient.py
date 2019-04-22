0adc4301143eb8bc6a3ffba5d35f8ce3954788da
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-22 12:46:19 +0800

    magic

--- code/core/webSocketClient.py
+++ code/core/webSocketClient.py
@@ -8,6 +8,8 @@ from twisted.internet import reactor
 IP = '127.0.0.1'
 PORT = 8008
 
+TIMEOUT = 0.1
+
 clientTypes = [
     'motor',
     'surface'
@@ -17,6 +19,7 @@ class ClientProtocol(WebSocketClientProtocol):
 
     def onConnect(self, response):
         print("Server connected: {0}".format(response.peer))
+        self.factory.register(self)
 
     def onConnecting(self, transport_details):
         print("Connecting; transport details: {}".format(transport_details))
@@ -29,13 +32,33 @@ class ClientProtocol(WebSocketClientProtocol):
             self.sendMessage("nothing")
             self.factory.reactor.callLater(1, sendData) # improve this
 
-        hello()
+        sendData()
 
     def onMessage(self, payload, isBinary):
         pass
 
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {0}".format(reason))
+        self.factory.unregister(self)
+
+class ClientFactory(WebSocketClientFactory):
+
+    def __init__(self, url):
+        WebSocketClientFactory.__init__(self, url)
+        self.connections = []
+
+    def register(self, client):
+        if client not in self.connections:
+            self.connections.append(client)
+
+    def unregister(self, client):
+        if client in self.connections:
+            self.connections.remove(client)
+
+
+    def broadcast(self, msg):
+        for c in self.connections:
+            c.sendMessage(b'Hello World!')
 
 
 log.startLogging(sys.stdout)
@@ -44,4 +67,8 @@ factory = WebSocketClientFactory(u'ws://{}:{}/surface'.format(IP , PORT))
 factory.protocol = ClientProtocol
 
 reactor.connectTCP(IP, PORT, factory)
+
+l = task.LoopingCall(server.broadcast)
+l.start(TIMEOUT)
+
 reactor.run()
