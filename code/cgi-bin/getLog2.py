ac8ff99571808c2ce6e5f627db0c5655161d2924
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-23 18:32:55 +0800

    log2

---
+++ code/cgi-bin/getLog2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.4
+import time
+
+def getDateISO8601():
+    tmp = time.localtime()
+    return '{}-{:0>2}-{:0>2}'.format(tmp.tm_year, tmp.tm_mon, tmp.tm_mday)
+
+print("Content-type: text/html\n\n")
+
+date = getDateISO8601()
+try:
+    f = open('/var/www/scripts/nohup.out.log', 'r')
+    raw = f.read()
+    f.close()
+
+    raw = raw.replace('\n', '<br>')
+
+    print(raw)
+except FileNotFoundError as e:
+    print(e)
