b5f9400b00311e8798868a8dcdbe53d795d3b6da
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-24 17:06:02 +0800

    major fixes cause im retarded

--- code/core/motor.py
+++ code/core/motor.py
@@ -77,23 +77,25 @@ def process(data):
 
     joystick1 = dict(zip(axis + buttons, joysticks[:12]))
     joystick2 = dict(zip(axis + buttons, joysticks[12:]))
-    joysticks = [joystick1, joystick2]
-    old = joystick1
 
-    print('msg:', joysticks)
+    old = [] # for debugging
+
+    #print('msg:', joysticks)
 
     del data
 
     global justPressed
 
-    for stick, jPressed in zip(joysticks, justPressed):
+    stickNum = 0
+    for stick, jPressed in zip((joystick1, joystick2), justPressed):
         for k in stick:
             if k not in buttons:
                 continue
+
             v = stick[k]
             if v == 1 and not jPressed[k]:
                 # just pressed
-                buttonPressed(k, joysticks.index(stick))
+                buttonPressed(k, stickNum)
                 jPressed[k] = True
             elif v == 0 and jPressed[k]:
                 # just released
@@ -103,120 +105,126 @@ def process(data):
             else:
                 pass
 
-        motor_claw = 90
-        joystick1 = joysticks[0]
+        stickNum += 1
 
-        print(joystick1)
-        if joystick1['A'] and joystick1['B']:
-            pass # do nothing cause both are pressed
-        elif joystick1['A']:
-            motor_claw = 150
-        elif joystick1['B']:
-            motor_claw = 30
+    del stickNum
 
+    motor_claw = 90
 
-
-        #       150    150
-        #       /a/    \b\
-        #  4  /////    \\\\\  2
-        #
-        #               150
-        #  5  \\\\\    /////  3
-        #      \c\     /d/
-        #      150
-        # ______________a_b_c_d
-        # Back          - - + +
-        # Front         + + - -
-        # Strafe Left   - + - +
-        # Strafe Right  + - + -
-        # Rotate Left   - + + -
-        # Rotate Right  + - - +
-        #
-
-        yLeft = 60 * joystick1['yLeft']
-        xLeft = 60 * joystick1['xLeft']
-        yRight = 60 * joystick1['yRight']
-
-        spin = 0
-
-        print(joystick1['triggerRight']) # equals 0 instead of one
-        joystick1['triggerRight'] = (joystick1['triggerRight'] + 1) / 2
-
-        joystick1['triggerLeft'] = (joystick1['triggerLeft'] + 1) / 2
-        print(joystick1['triggerRight'])
-
-        if joystick1['triggerRight'] >= 0.1 and joystick1['triggerLeft'] >= 0.1:
-            pass # do nothing cause both are pressed
-        else:
-            if joystick1['triggerRight'] > 0.1:
-                # spin right
-                spin = joystick1['triggerRight'] * 60
-
-            if joystick1['triggerLeft'] > 0.1:
-                # spin left
-                spin = -joystick1['triggerLeft'] * 60
+    print(joystick1)
+    if joystick1['A'] and joystick1['B']:
+        pass # do nothing cause both are pressed
+    elif joystick1['A']:
+        motor_claw = 150
+    elif joystick1['B']:
+        motor_claw = 30
 
 
-        motor_a = 90 + yLeft - xLeft - spin
 
-        motor_b = 90 + yLeft + xLeft + spin
+    #       150    150
+    #       /a/    \b\
+    #  4  /////    \\\\\  2
+    #
+    #               150
+    #  5  \\\\\    /////  3
+    #      \c\     /d/
+    #      150
+    # ______________a_b_c_d
+    # Back          - - + +
+    # Front         + + - -
+    # Strafe Left   - + - +
+    # Strafe Right  + - + -
+    # Rotate Left   - + + -
+    # Rotate Right  + - - +
+    #
 
-        motor_c = 90 - yLeft + xLeft - spin
+    yLeft = 60 * joystick1['yLeft']
+    xLeft = 60 * joystick1['xLeft']
+    yRight = 60 * joystick1['yRight']
 
-        motor_d = 90 - yLeft - xLeft + spin
+    spin = 0
 
-        #mixing
-        #m1 = 90+forwardval+strafeval+turnval
-        #m2 = 90+forwardval-strafeval-turnval
-        #m3 = 90-forwardval+strafeval
-        #m4 = 90-forwardval-strafeval
+    joystick1['triggerRight'] = (joystick1['triggerRight'] + 1) / 2
+    joystick1['triggerLeft'] = (joystick1['triggerLeft'] + 1) / 2
 
-        global trimUp
+    joystick2['triggerRight'] = (joystick2['triggerRight'] + 1) / 2
+    joystick2['triggerLeft'] = (joystick2['triggerLeft'] + 1) / 2
 
-        motor_up_left  = 93 + trimUp['left'] + yRight
-        motor_up_right = 93 + trimUp['right'] + yRight
+    if joystick1['triggerRight'] >= 0.1 and joystick1['triggerLeft'] >= 0.1:
+        pass # do nothing cause both are pressed
+    else:
+        if joystick1['triggerRight'] > 0.1:
+            # spin right
+            spin = joystick1['triggerRight'] * 60
 
-        def bounds(x):
-            if x < 30:
-                return 30
-            if x > 150:
-                return 150
-            return round(x, 2)
+        if joystick1['triggerLeft'] > 0.1:
+            # spin left
+            spin = -joystick1['triggerLeft'] * 60
 
-        motor_a = bounds(motor_a)
-        motor_b = bounds(motor_b)
-        motor_c = bounds(180 - motor_c) # reverse
-        motor_d = bounds(motor_d)
 
-        motor_up_left  = bounds(motor_up_left)
-        motor_up_right = bounds(motor_up_right)
+    motor_a = 90 + yLeft - xLeft - spin
 
-        '''
-        # right
-        pins[4].write(motor_a)
-        pins[2].write(motor_b)
-        pins[3].write(motor_c)
-        pins[5].write(motor_d)
+    motor_b = 90 + yLeft + xLeft + spin
 
-        # 150 down up, 30 move up
-        pins[6].write(motor_up_right) # stop on 93
-        pins[7].write(motor_up_left) # stop on 93
+    motor_c = 90 - yLeft + xLeft - spin
 
-        pins[8].write(motor_claw)
-        '''
+    motor_d = 90 - yLeft - xLeft + spin
 
-        # print datalist
-        for i in range(30):
-            print('\r\033[A\033[K', end='')
+    #mixing
+    #m1 = 90+forwardval+strafeval+turnval
+    #m2 = 90+forwardval-strafeval-turnval
+    #m3 = 90-forwardval+strafeval
+    #m4 = 90-forwardval-strafeval
 
-        print('Trim: [{0}, {1}]'.format(trimUp['left'], trimUp['right']))
-        print(joysticks[0])
-        print(joysticks[1])
-        print(motor_a, motor_b)
-        print(motor_c, motor_d)
-        print()
-        print(motor_up_left, motor_up_right)
-        print(motor_claw)
-        print(old)
+    global trimUp
+
+    motor_up_left  = 93 + trimUp['left'] + yRight
+    motor_up_right = 93 + trimUp['right'] + yRight
+
+    def bounds(x):
+        if x < 30:
+            return 30
+        if x > 150:
+            return 150
+        return round(x, 2)
+
+    motor_a = bounds(motor_a)
+    motor_b = bounds(motor_b)
+    motor_c = bounds(180 - motor_c) # reverse
+    motor_d = bounds(motor_d)
+
+    motor_up_left  = bounds(motor_up_left)
+    motor_up_right = bounds(motor_up_right)
+
+    '''
+    # right
+    pins[4].write(motor_a)
+    pins[2].write(motor_b)
+    pins[3].write(motor_c)
+    pins[5].write(motor_d)
+
+    # 150 down up, 30 move up
+    pins[6].write(motor_up_right) # stop on 93
+    pins[7].write(motor_up_left) # stop on 93
+
+    pins[8].write(motor_claw)
+    '''
+
+    # print datalist
+    for i in range(30):
+        print('\r\033[A\033[K', end='')
+
+    print('Trim: [{0}, {1}]'.format(trimUp['left'], trimUp['right']))
+    print(joystick1)
+    print(joystick2)
+    print(motor_a, motor_b)
+    print(motor_c, motor_d)
+    print()
+    print(motor_up_left, motor_up_right)
+    print(motor_claw)
+    index = 0
+    for i in old:
+        print(index, i)
+        index += 1
 
 webSocketClient.start('motor', process)
