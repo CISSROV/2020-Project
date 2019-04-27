aff0d930dd51cfffc53df3238114d98c21ae7a74
hydrogen602 <32135041+hydrogen602@users.noreply.github.com>
2019-04-27 14:42:31 +0800

    reversing rotation

--- code/web/heading/heading.js
+++ code/web/heading/heading.js
@@ -5,7 +5,7 @@ var indicator = document.getElementById('heading_indicator')
 var degs = 0
 
 function rotate(degree) {
-    degree = Math.abs(degree)
+    degree = Math.abs(360-degree)
     if (degree < 0 || degree > 360) {
         document.getElementById('error').hidden = false
         throw new Error('Degree Argument out of Range, got ' + degree)
