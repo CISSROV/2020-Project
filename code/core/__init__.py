d777424b8a519279fa7163a0c5027090da4259d2
Jonathan Rotter <hydrogen31415@gmail.com>
2020-01-09 16:12:08 -0600

    more docs

--- code/core/__init__.py
+++ code/core/__init__.py
@@ -1,3 +1,9 @@
+'''
+The core part of the ROV software, connecting the
+XBox controller of the base station to the motors
+of the ROV. All movement control and communication
+happens in this module.
+'''
 
 import sys
 print(sys.path[0])
